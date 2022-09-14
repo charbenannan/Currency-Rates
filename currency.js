7e5bf7a225c3a8acff12f0eb94a7f1dbd566f155
Charles Annan <79229028+stiffchoco@users.noreply.github.com>
2022-09-14 19:58:55 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,108 +1,127 @@
-// const { response } = require("har-validator");
+ let finalData = null;
+      const pairs = [];
+      async function fetchRateData(currency) {
+        const pairs = [];
+        $("#group1").empty();
+        $("#group2").empty();
+        $("#group3").empty();
+        $("#group1").append(" <h1>Group1</h1>")
+        $("#group2").append(" <h1>Group2</h1>")
+        $("#group3").append(" <h1>Group3</h1>")
+        let currencies = ["AUD", "BGN", "CAD", "CHF", "EUR", "USD", "NZD"];
+        currencies = currencies.filter((item) => item !== currency);
 
-document.addEventListener("DOMContentLoaded", () =>{ 
- const currency = document.querySelector('#rates').value;
- 
- let apiRequest =
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/nzd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/bgn.json')
-  
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/nzd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/bgn.json')
+        const groups = [
+          {
+            name: "AUD",
+            alternates: ["BGN", "CAD", "CHF", "EUR", "USD", "NZD"],
+          },
+          {
+            name: "BGN",
+            alternates: ["AUD", "CAD", "CHF", "EUR", "USD", "NZD"],
+          },
+          {
+            name: "CAD",
+            alternates: ["AUD", "BGN", "CHF", "EUR", "USD", "NZD"],
+          },
+          {
+            name: "CHF",
+            alternates: ["AUD", "BGN", "CAD", "EUR", "USD", "NZD"],
+          },
+          {
+            name: "EUR",
+            alternates: ["AUD", "BGN", "CAD", "CHF", "USD", "NZD"],
+          },
+          {
+            name: "USD",
+            alternates: ["AUD", "BGN", "CAD", "CHF", "EUR", "NZD"],
+          },
+          {
+            name: "NZD",
+            alternates: ["AUD", "CAD", "BGN", "CHF", "EUR", "USD"],
+          },
+        ];
+        let requestPromise = [];
+        await groups
+          .filter((item) => item.name === currency)
+          .forEach(async (e) => {
+            let baseName = e.name;
+            let ratePairs = [];
+            e.alternates.forEach(async (o) => {
+              const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${baseName.toLowerCase()}/${o.toLowerCase()}.json`;
+              const response = fetch(encodeURI(url));
 
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/nzd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/aud/bgn.json')
+              ratePairs.push(response);
+            });
+            // const rr = await Promise.all(ratePairs);
+            pairs.push({ name: baseName, ratePairs });
+          });
+        const resolveAll = await Promise.all(pairs[0].ratePairs);
 
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/nzd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad/bgn.json')
+        let fd = [];
+        let g1 = [];
+        let g2 = [];
+        let g3 = [];
+        // console.log(resolveAll)
+        resolveAll.forEach(async (e, index) => {
+          let resp = e.json();
+          fd.push(resp);
+        });
+        fd = await Promise.all(fd);
 
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/nzd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/chf/bgn.json')
+        fd.forEach(async (e, index) => {
+          const rate = e[currencies[index].toLowerCase()];
+          //   console.log(rate)
+          //   fd.push({ rate, c: currencies[index] });
 
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd/bgn.json')
+          console.log(rate);
+          if (rate < 1) {
+            console.log("less than 1", rate);
+            g1.push({ base: currency, value: rate, to: currencies[index], });
+          }
+          if (rate > 1 || rate < 1.5) {
+            g2.push({ base: currency, value: rate, to: currencies[index], });
+          }
+          if (rate > 1.5) {
+            g3.push({
+              base: currency,
+              value: rate,
+              to: currencies[index],
+            });
+          }
+          finalData = { g1, g2, g3 };
+          //   console.log(resp, index);
+        });
 
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/usd.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/eur.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/aud.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/cad.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/chf.json')
- fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/bgn/nzd.json')
+        // console.log(fd, "fd");
+        // console.log(g1,"g1")
 
-apiRequest.then((response) =>{
-    
-    return response.json();
-})
-.then ((data) =>{
-    console.log(data) 
+        //render group 1
+        finalData.g1.map((item) =>
+          $("#group1").append(`<div>${item.base}-${item.to} : ${item.value}</div>`)
+        );
+        $("#group1").append(`<p>Count : ${finalData.g1.length}</p>`)
+        finalData.g2.map((item) =>
+          $("#group2").append(`<div>${item.base}-${item.to} : ${item.value}</div>`)
+        );
+        $("#group2").append(`<p>Count : ${finalData.g2.length}</p>`)
+        finalData.g3.map((item) =>
+          $("#group3").append(`<div>${item.base}-${item.to} : ${item.value}</div>`)
+        );
+        $("#group3").append(`<p>Count : ${finalData.g3.length}</p>`)
+        console.log(finalData, "final");
 
-    // Group currency pair for USD
-    
-    document.getElementById('rates').addEventListener('change', function groupOne(event) {
-    
-        if(event.target.value == 'USD'){
-       for(var i=0; i<data.length; i++){
-        document.getElementById("pair-1").innerHTML += 
-        data[i].id +' ' + data[i].eur + " <br />"
-       }
-     }
-           
-    })
- 
-})
+        // pairs[0].ratePairs.forEach((element, index) => {
+        //   console.log("dfd");
+        //   $("#output").append(`<div>${element[currencies[index]]}666</div>`);
+        // });
+        // console.log(resolveAll, "here");
+      }
 
-});
+      //    fetchRateData()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      async function onChangeCurrency() {
+        console.log("jjj");
+        var x = document.getElementById("rates").value;
+        await fetchRateData(x);
+      }
